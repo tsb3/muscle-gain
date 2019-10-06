b50e667c0d2e390eb560b6487beeafa273c6a42f
tsb3 <tsubasa.ito111@gmail.com>
2019-10-06 16:52:54 +0900

    Add categories model and tests

---
+++ spec/controllers/categories_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe CategoriesController do
+  describe 'GET #index' do
+  end
+
+  describe 'GET #new' do
+  
+  end
+
+  describe 'GET #show' do
+  end
+end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,17 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe 'valid' do
+RSpec.describe 'validate' do
+  
   it 'category should be valid' do
-    @category = Category.new(name: "sports")
-    @category.valid?
+    category = Category.new(name: "sports")
+    expect(category).to be_valid
   end
 
   it 'category should be present' do
-    category[column_name] = ''
+    category = Category.new(name: " ")
     expect(category).not_to be_valid
   end
 
+  it 'category should be unique' do
+    category = Category.new(name: "sports")
+    category.save
+    category2 = Category.new(name: "sports")
+    expect(category2).not_to be_valid
+  end
+  
+  it 'category should be unique' do
+    category = Category.new(name: "sports")
+    category.name = "a" * 26
+    expect(category).not_to be_valid
+  end
 
-
+  it 'category should be unique' do
+    category = Category.new(name: "sports")
+    category.name = "aa"
+    expect(category).not_to be_valid
+  end
 
 end
